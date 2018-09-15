efd735e974e9b4fa53f574ad1c6afb5045bd404c
Muttsu <22158218+Muttsu@users.noreply.github.com>
2018-09-15 16:02:04 -0400

    firebase implementation v2

--- app/src/main/java/com/mooncakestudio/corridor/snapevnt/App.java
+++ app/src/main/java/com/mooncakestudio/corridor/snapevnt/App.java
@@ -1,4 +1,13 @@
 package com.mooncakestudio.corridor.snapevnt;
 
-public class App {
+import android.app.Application;
+
+public class App extends Application{
+    private User currentUser = new User("service_u0","Service Account #1", "N/A");
+    public User getCurrentUser() {
+        return new User("service_u0","Service_Account#1", "N/A");
+    }
+    private void setCurrentUser(User user) {
+        currentUser = user;
+    }
 }
